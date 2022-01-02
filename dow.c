4e30ca078f1b9d7ad844bf6bae06d111ced28aa0
Ergoold <eran1gold@gmail.com>
2022-01-02 18:00:19 +0200

    optimize dow

--- dow.c
+++ dow.c
@@ -25,14 +25,12 @@ static const int months[] = { 1, 4, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6 };
 int dow(int y, int m, int d)
 {
 	int yr = y % 100;
-	int yr4 = yr / 4;
-	int yr4d = yr4 + d;
-	int yr4dm = yr4d + months[m - 1];
+	int dow = yr / 4 + d + months[m - 1];
 	int isleap = y % 4 == 0 && (y % 100 != 0 || y % 400 == 0);
-	if (isleap && m <= 2 ) yr4dm--;
-	if ((y / 100) % 4 == 0) yr4dm += 6;
-	if ((y / 100) % 4 == 1) yr4dm += 2;
-	if ((y / 100) % 4 == 2) yr4dm += 4;
-	yr4dm += yr;
-	return yr4dm % 7;
+	if (isleap && m <= 2 ) dow--;
+	int coe = y / 100 % 4;
+	if (coe == 0) dow += 6;
+	if (coe == 1) dow += 2;
+	if (coe == 2) dow += 4;
+	return (dow + yr) % 7;
 }
